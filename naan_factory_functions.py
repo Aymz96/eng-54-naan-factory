1fbad8ab0256e1ef02ea0f8856a71e33a492e566
Tech-A23 <tech-a23@Tech-A23s-MacBook-Air.local>
2020-03-05 17:15:20 +0000

    did some test

---
+++ naan_factory_functions.py
@@ -0,0 +1,46 @@
+# Factory functions
+# define our function  here
+from run_naan_factory import *
+
+def make_dough(arg1, arg2):
+    # if argument is water and argument 2 is flour
+        # return doght
+    # else return
+        # not dough
+    return 'dough'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# def test_example_2():
+#     return 'Greating TESTING!'
+#
+# def test_example():
+#     return 'TESTING!'
+#
+#
+# #
+# # print(test_example_2())
+# #
+# # print('anything')
+#
+# # don't call function here!
+# # this will violate the separation of concerns and
+# # it will run said functions when you are importing them elsewhere
+
